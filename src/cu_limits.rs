dbd863b10db14385cb923c5eb631ca6e1a802483
jovicheng <jovicheng@foxmail.com>
2024-04-05 02:09:55 +0800

    fix

--- src/cu_limits.rs
+++ src/cu_limits.rs
@@ -1,4 +1,4 @@
-pub const CU_LIMIT_REGISTER: u32 = 7660;
+pub const CU_LIMIT_REGISTER: u32 = 15_000;
 // pub const CU_LIMIT_CLAIM: u32 = 11_000;
 // pub const CU_LIMIT_TRANSFER: u32 = 30_000; // TODO
 pub const CU_LIMIT_ATA: u32 = 240_000;
